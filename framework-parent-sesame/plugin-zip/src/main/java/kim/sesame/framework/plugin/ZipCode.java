162e20efbd4cf84d8e84d3f7fa557524a69b9c28
johnny <654499437@qq.com>
2018-02-28 19:22:42 +0800

    自定义插件

---
+++ framework-parent-sesame/plugin-zip/src/main/java/kim/sesame/framework/plugin/ZipCode.java
@@ -0,0 +1,20 @@
+package kim.sesame.framework.plugin;
+
+
+import lombok.extern.apachecommons.CommonsLog;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * 压缩代码
+ **/
+@CommonsLog
+public class ZipCode extends AbstractMojo {
+
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        log.info("11111111111111111111111");
+    }
+
+}
