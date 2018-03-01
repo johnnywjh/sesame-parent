d9facd6a97cccea1fff1417b49a2c8fb7651d1b1
johnny <654499437@qq.com>
2018-03-01 10:32:20 +0800

    自定义maven插件,压缩资源文件

--- framework-parent-sesame/plugin-zip/src/main/java/kim/sesame/framework/plugin/ZipCode.java
+++ framework-parent-sesame/plugin-zip/src/main/java/kim/sesame/framework/plugin/ZipCode.java
@@ -1,20 +1,42 @@
 package kim.sesame.framework.plugin;
 
 
-import lombok.extern.apachecommons.CommonsLog;
+import kim.sesame.framework.uitls.ZipCompressor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
 
 /**
  * 压缩代码
  **/
-@CommonsLog
+@Mojo(name = "zip")
 public class ZipCode extends AbstractMojo {
 
+    @Parameter
+    private String path;
 
     @Override
     public void execute() throws MojoExecutionException {
-        log.info("11111111111111111111111");
+        getLog().info("执行压缩插件..................");
+        if (path != null && path.length() > 0) {
+            File file = new File(path);
+            if (file.exists()) {
+                String fileName = path.substring(path.lastIndexOf("/") + 1);
+                // 文件压缩
+                ZipCompressor zc = new ZipCompressor(path + ".zip");
+                zc.compress(path);
+                getLog().info("文件压缩成功 : " + path + ".zip");
+            } else {
+                getLog().warn("path 路径不存在, : " + path);
+            }
+        } else {
+            getLog().warn("path 路径不能为空");
+        }
+        getLog().info("执行压缩插件结束..................");
     }
 
 }
