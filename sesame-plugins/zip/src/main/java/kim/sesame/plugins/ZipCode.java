6eac8bcb04cdd10412b7ef6bf491a09179edc3e5
johnny <654499437@qq.com>
2018-03-08 12:27:48 +0800

    版本更新 1.2

--- sesame-plugins/zip/src/main/java/kim/sesame/plugins/ZipCode.java
+++ sesame-plugins/zip/src/main/java/kim/sesame/plugins/ZipCode.java
@@ -4,11 +4,11 @@ package kim.sesame.plugins;
 import kim.sesame.framework.uitls.ZipCompressor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * 压缩代码
@@ -17,25 +17,29 @@ import java.io.File;
 public class ZipCode extends AbstractMojo {
 
     @Parameter
-    private String path;
+    private List<String> paths;
 
     @Override
     public void execute() throws MojoExecutionException {
         getLog().info("执行压缩插件..................");
-        if (path != null && path.length() > 0) {
-            File file = new File(path);
-            if (file.exists()) {
-                String fileName = path.substring(path.lastIndexOf("/") + 1);
-                // 文件压缩
-                ZipCompressor zc = new ZipCompressor(path + ".zip");
-                zc.compress(path);
-                getLog().info("文件压缩成功 : " + path + ".zip");
+        for (int i = 0; i < paths.size(); i++) {
+            String path = paths.get(i);
+            if (path != null && path.length() > 0) {
+                File file = new File(path);
+                if (file.exists()) {
+                    String fileName = path.substring(path.lastIndexOf("/") + 1);
+                    // 文件压缩
+                    ZipCompressor zc = new ZipCompressor(path + ".zip");
+                    zc.compress(path);
+                    getLog().info("文件压缩成功 : " + path + ".zip");
+                } else {
+                    getLog().warn("path 路径不存在, : " + path);
+                }
             } else {
-                getLog().warn("path 路径不存在, : " + path);
+                getLog().warn("path 路径不能为空");
             }
-        } else {
-            getLog().warn("path 路径不能为空");
         }
+
         getLog().info("执行压缩插件结束..................");
     }
 
