52504929c5f4a76f2f37788da7f9e2ff6488d943
johnny <654499437@qq.com>
2018-03-20 15:44:50 +0800

    Ztree

--- framework-parent/framework-core/src/main/java/kim/sesame/framework/entity/ZTree.java
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/entity/ZTree.java
@@ -6,10 +6,9 @@ import lombok.Data;
  * 菜单权限控制
  * @author johnny
  * date :  2016年1月17日 下午4:17:39
- * Description:
  */
 @Data
-public class Ztter implements java.io.Serializable {
+public class ZTree implements java.io.Serializable {
 	
 	private Integer id; 
 	private Integer pId;
@@ -18,11 +17,11 @@ public class Ztter implements java.io.Serializable {
 	private boolean open; //是否展开
 	private boolean chkDisabled;//是否禁用,true 禁用
 
-	public Ztter() {
+	public ZTree() {
 		super();
 	}
 	
-	public Ztter(Integer id, Integer pId, String name, boolean open) {
+	public ZTree(Integer id, Integer pId, String name, boolean open) {
 		super();
 		this.id = id;
 		this.pId = pId;
@@ -30,7 +29,7 @@ public class Ztter implements java.io.Serializable {
 		this.open = open;
 	}
 
-	public Ztter(Integer id, Integer pId, String name, boolean checked, boolean open, boolean chkDisabled) {
+	public ZTree(Integer id, Integer pId, String name, boolean checked, boolean open, boolean chkDisabled) {
 		super();
 		this.id = id;
 		this.pId = pId;
