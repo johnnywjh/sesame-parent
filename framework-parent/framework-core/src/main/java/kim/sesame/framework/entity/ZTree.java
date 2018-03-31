179f29dfd27053a11a3bee0aef56680d579386fd
johnny <654499437@qq.com>
2018-03-31 10:39:25 +0800

    分布式事物模块 依赖升级

--- framework-parent/framework-core/src/main/java/kim/sesame/framework/entity/ZTree.java
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/entity/ZTree.java
@@ -10,34 +10,12 @@ import lombok.Data;
 @Data
 public class ZTree implements java.io.Serializable {
 	
-	private Integer id; 
-	private Integer pId;
+	private String id;
+	private String parentId;
 	private String name;
 	private boolean checked; //是否选中
 	private boolean open; //是否展开
 	private boolean chkDisabled;//是否禁用,true 禁用
-
-	public ZTree() {
-		super();
-	}
-	
-	public ZTree(Integer id, Integer pId, String name, boolean open) {
-		super();
-		this.id = id;
-		this.pId = pId;
-		this.name = name;
-		this.open = open;
-	}
-
-	public ZTree(Integer id, Integer pId, String name, boolean checked, boolean open, boolean chkDisabled) {
-		super();
-		this.id = id;
-		this.pId = pId;
-		this.name = name;
-		this.checked = checked;
-		this.open = open;
-		this.chkDisabled = chkDisabled;
-	}
 
 	
 }
--- framework-parent/framework-distributed-tx/src/main/java/kim/sesame/framework/tx/config/TxDataSource.java
+++ framework-parent/framework-distributed-tx/src/main/java/kim/sesame/framework/tx/config/TxDataSource.java
@@ -1,43 +1,34 @@
 package kim.sesame.framework.tx.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.codingapi.tx.datasource.relational.LCNTransactionDataSource;
-import com.codingapi.tx.springcloud.feign.TransactionRestTemplateInterceptor;
-import feign.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import javax.sql.DataSource;
+
 @Configuration
-public class TXConfig {
+public class TxDataSource {
 
     @Autowired
     private Environment env;
 
     @Bean
-    public RequestInterceptor requestInterceptor(){
-        return new TransactionRestTemplateInterceptor();
-    }
-
-    @Bean
-    public LCNTransactionDataSource dataSource() {
+    public DataSource dataSource() {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));//用户名
         dataSource.setPassword(env.getProperty("spring.datasource.password"));//密码
-        dataSource.setInitialSize(2);
-        dataSource.setMaxActive(20);
+        dataSource.setInitialSize(10);
+        dataSource.setMaxActive(50);
         dataSource.setMinIdle(0);
         dataSource.setMaxWait(60000);
         dataSource.setValidationQuery("SELECT 1");
         dataSource.setTestOnBorrow(false);
         dataSource.setTestWhileIdle(true);
         dataSource.setPoolPreparedStatements(false);
-
-        LCNTransactionDataSource dataSourceProxy = new LCNTransactionDataSource();
-        dataSourceProxy.setDataSource(dataSource);
-        dataSourceProxy.setMaxCount(10);
-        return dataSourceProxy;
+        return dataSource;
     }
+
 }
