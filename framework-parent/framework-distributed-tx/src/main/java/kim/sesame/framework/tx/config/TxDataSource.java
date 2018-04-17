8a744640768631a9e7be912511a61d56811a7d95
johnny <654499437@qq.com>
2018-04-17 19:53:24 +0800

    数据库配置增加加密配置

--- framework-parent/framework-distributed-tx/src/main/java/kim/sesame/framework/tx/config/TxDataSource.java
+++ framework-parent/framework-distributed-tx/src/main/java/kim/sesame/framework/tx/config/TxDataSource.java
@@ -1,11 +1,13 @@
 package kim.sesame.framework.tx.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import kim.sesame.framework.mybatis.config.DruidProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
@@ -13,18 +15,27 @@ public class TxDataSource {
 
     @Autowired
     private Environment env;
+    @Resource
+    private DruidProperties druid;
 
     @Bean
     public DataSource dataSource() {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));//用户名
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));//密码
-        dataSource.setInitialSize(10);
-        dataSource.setMaxActive(50);
-        dataSource.setMinIdle(0);
-        dataSource.setMaxWait(60000);
-        dataSource.setValidationQuery("SELECT 1");
+        String username = env.getProperty("spring.datasource.username");
+        String password = env.getProperty("spring.datasource.password");
+        if(druid.isEncryption()){
+            username = druid.decodeStr(username);
+            password = druid.decodeStr(password);
+        }
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaxActive(druid.getMaxActive());
+        dataSource.setInitialSize(druid.getInitialSize());
+
+        dataSource.setMinIdle(druid.getMinIdle());
+        dataSource.setMaxWait(druid.getMaxWait());
+        dataSource.setValidationQuery("select 'x'");
         dataSource.setTestOnBorrow(false);
         dataSource.setTestWhileIdle(true);
         dataSource.setPoolPreparedStatements(false);
