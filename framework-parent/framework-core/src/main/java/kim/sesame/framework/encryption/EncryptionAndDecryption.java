02e3117a7187f8eb6cad9686deccfca54235a6c7
johnny <654499437@qq.com>
2018-05-03 10:37:24 +0800

    加密解密修改

--- framework-parent/framework-core/src/main/java/kim/sesame/framework/encryption/EncryptionAndDecryption.java
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/encryption/EncryptionAndDecryption.java
@@ -8,31 +8,45 @@ import javax.crypto.spec.IvParameterSpec;
 import java.security.Key;
 
 /**
- * Created by mlick on 2017-06-17. des3 两种加密方式 ECB和CBC模式
+ * 对称加密,解密
  */
-public class DES3Utils {
+public class EncryptionAndDecryption {
 
     private static byte[] keyiv = {1, 2, 3, 4, 5, 6, 7, 8};
-    private static String keysecret = "kimsesame-123456789123456789123456789";
     public final static String ENCODE = "UTF-8";
 
+    private String keysecret;
 
-    public static void setKeysecret(String keysecret) {
-        DES3Utils.keysecret = keysecret;
+    public EncryptionAndDecryption() {
+        keysecret = "kimsesame-123456789123456789123456789";
     }
 
-    public static String encodeECB(String data) throws Exception {
-        byte[] key = keysecret.getBytes(ENCODE);
-        byte[] str9 = des3EncodeECB(key, data.getBytes(ENCODE));
-        return new String(Base64.encode(str9, Base64.NO_WRAP), ENCODE);
+    public EncryptionAndDecryption(String keysecret) {
+        this.keysecret = keysecret;
     }
 
-    public static String decodeECB(String data) throws Exception {
-        byte[] key = keysecret.getBytes(ENCODE);
-        return new String(des3DecodeECB(key, Base64.decode(data, Base64.NO_WRAP)), ENCODE);
+    public void setKeysecret(String keysecret) {
+        this.keysecret = keysecret;
     }
 
-    public static String decodeCBC(String data) {
+    /**
+     * CBC加密
+     */
+    public String encodeCBC(String data) {
+        try {
+            byte[] key = keysecret.getBytes(ENCODE);
+            byte[] str7 = des3EncodeCBC(data.getBytes(ENCODE));
+            return new String(Base64.encode(str7, Base64.NO_WRAP), ENCODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * CBC解密
+     */
+    public String decodeCBC(String data) {
         try {
             byte[] key = keysecret.getBytes(ENCODE);
             byte[] str8 = des3DecodeCBC(Base64.decode(data, Base64.NO_WRAP));
@@ -44,17 +58,36 @@ public class DES3Utils {
     }
 
     /**
-     * ECB加密,不要IV
-     *
-     * @param data 明文
-     * @return Base64编码的密文
-     * @throws Exception 异常
+     * ECB加密
      */
-    public static String encodeCBC(String data) throws Exception {
-        byte[] key = keysecret.getBytes(ENCODE);
-        byte[] str7 = des3EncodeCBC(data.getBytes(ENCODE));
-        return new String(Base64.encode(str7, Base64.NO_WRAP), ENCODE);
+    public String encodeECB(String data) {
+        try {
+            byte[] key = keysecret.getBytes(ENCODE);
+            byte[] str9 = des3EncodeECB(key, data.getBytes(ENCODE));
+            return new String(Base64.encode(str9, Base64.NO_WRAP), ENCODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
+    /**
+     * ECB解密
+     */
+    public String decodeECB(String data) {
+        try {
+            byte[] key = keysecret.getBytes(ENCODE);
+            return new String(des3DecodeECB(key, Base64.decode(data, Base64.NO_WRAP)), ENCODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /*-----------------------------------------------------------------------*/
+    /*----------    分割线,内部方法   ---------------------------------*/
+    /*-----------------------------------------------------------------------*/
 
     /**
      * ECB加密,不要IV
@@ -64,7 +97,7 @@ public class DES3Utils {
      * @return Base64编码的密文
      * @throws Exception 异常
      */
-    public static byte[] des3EncodeECB(byte[] key, byte[] data) throws Exception {
+    private byte[] des3EncodeECB(byte[] key, byte[] data) throws Exception {
         Key deskey = null;
         DESedeKeySpec spec = new DESedeKeySpec(key);
         SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
@@ -83,7 +116,7 @@ public class DES3Utils {
      * @return 明文
      * @throws Exception 异常
      */
-    public static byte[] des3DecodeECB(byte[] key, byte[] data) throws Exception {
+    private byte[] des3DecodeECB(byte[] key, byte[] data) throws Exception {
         Key deskey = null;
         DESedeKeySpec spec = new DESedeKeySpec(key);
         SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
@@ -96,13 +129,14 @@ public class DES3Utils {
 
     /**
      * CBC加密
+     *
      * @param data btye arr
-     * @return  byte arr
+     * @return byte arr
      * @throws Exception 异常
      */
-    public static byte[] des3EncodeCBC(byte[] data) throws Exception {
+    private byte[] des3EncodeCBC(byte[] data) throws Exception {
         Key deskey = null;
-        DESedeKeySpec spec = new DESedeKeySpec(DES3Utils.keysecret.getBytes(ENCODE));
+        DESedeKeySpec spec = new DESedeKeySpec(keysecret.getBytes(ENCODE));
         SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
         deskey = keyfactory.generateSecret(spec);
         Cipher cipher = Cipher.getInstance("desede" + "/CBC/PKCS5Padding");
@@ -114,13 +148,14 @@ public class DES3Utils {
 
     /**
      * CBC解密
+     *
      * @param data byte arr
      * @return btye arr
      * @throws Exception 异常
      */
-    public static byte[] des3DecodeCBC(byte[] data) throws Exception {
+    private byte[] des3DecodeCBC(byte[] data) throws Exception {
         Key deskey = null;
-        DESedeKeySpec spec = new DESedeKeySpec(DES3Utils.keysecret.getBytes(ENCODE));
+        DESedeKeySpec spec = new DESedeKeySpec(keysecret.getBytes(ENCODE));
         SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
         deskey = keyfactory.generateSecret(spec);
         Cipher cipher = Cipher.getInstance("desede" + "/CBC/PKCS5Padding");
--- framework-parent/framework-core/src/main/java/kim/sesame/framework/encryption/Test.java
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/encryption/Test.java
@@ -2,19 +2,31 @@ package kim.sesame.framework.encryption;
 
 
 public class Test {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		String str = "中国ABCabc123";
+        EncryptionAndDecryption ead = new EncryptionAndDecryption();
 
+        String str = "中国ABCabc123=";
+        System.out.println("加密前字符串:" + str);
 
-		System.out.println("CBC加密解密");
+        cbc(str,ead);
+        System.out.println("--------------------------------------");
+        ecb(str,ead);
 
-		System.out.println("加密前字符串:" + str);
+    }
 
-		String str1 = DES3Utils.encodeCBC(str);
-		System.out.println("加密后字符串:" + str1);
+    public static void cbc(String str, EncryptionAndDecryption ead) {
 
-		System.out.println("解密后字符串:" + DES3Utils.decodeCBC(str1));
+        System.out.println("ECB加密解密");
+        String str1 = ead.encodeECB(str);
+        System.out.println("加密后字符串:" + str1);
+        System.out.println("解密后字符串:" + ead.decodeECB(str1));
+    }
+    public static void ecb(String str, EncryptionAndDecryption ead) {
+        System.out.println("ECB加密解密");
+        String str1 = ead.encodeECB(str);
+        System.out.println("加密后字符串:" + str1);
+        System.out.println("解密后字符串:" + ead.decodeECB(str1));
+    }
 
-	}
 }
