185ec860bae5072519792c62eae02aeb1b83f121
johnny <654499437@qq.com>
2018-09-06 16:36:27 +0800

    日志记录模块, 添加配置检查

--- framework-parent/framework-tablelog/src/main/java/kim/sesame/framework/tablelog/config/TableOpProperties.java
+++ framework-parent/framework-tablelog/src/main/java/kim/sesame/framework/tablelog/config/TableOpProperties.java
@@ -1,6 +1,8 @@
 package kim.sesame.framework.tablelog.config;
 
+import kim.sesame.framework.utils.StringUtil;
 import lombok.Data;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +12,7 @@ import java.util.List;
 @Component
 @ConfigurationProperties(prefix = "sesame.framework.tablelog")
 //@PropertySource(value="classpath:config/application.properties",encoding = "UTF-8")
-public class TableOpProperties {
+public class TableOpProperties implements InitializingBean {
 
     /**
      * 是否开启, 默认关闭
@@ -36,4 +38,37 @@ public class TableOpProperties {
         return obj;
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 属性加载完成之后的操作
+
+        // 如果java属性和 数据库字段一样, 可以只写数据库字段
+        if (this.enable == false) {
+            return;
+        }
+        if (this.list == null || this.list.size() == 0) {
+            return;
+        }
+        for (OpTable ot : this.list) {
+            List<OpColumn> columnList = ot.getColumnList();
+            if (columnList == null || columnList.size() == 0) {
+                continue;
+            }
+            for (OpColumn c : columnList) {
+                boolean flg_db = StringUtil.isNotEmpty(c.getColumnName());
+                boolean flg_java = StringUtil.isNotEmpty(c.getJavaName());
+                if (flg_db && flg_java) {
+                    continue;
+                }
+                if (flg_db == false && flg_java == false) {
+                    throw new NullPointerException("请正确配置java属性和数据库字段属性");
+                }
+                if (flg_db) {
+                    c.setJavaName(c.getColumnName());
+                } else {
+                    c.setColumnName(c.getJavaName());
+                }
+            }
+        }
+    }
 }
