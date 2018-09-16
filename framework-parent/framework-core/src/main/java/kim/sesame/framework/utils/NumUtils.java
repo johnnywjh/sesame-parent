231f6e6ec78e5ae0a54eeb082ccb26e22ea0c9be
mlick <956217275@qq.com>
2018-09-16 20:19:02 +0800

    提交 订单工具类

--- framework-parent/framework-core/src/main/java/kim/sesame/framework/utils/NumUtils.java
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/utils/NumUtils.java
@@ -1,16 +1,28 @@
 package kim.sesame.framework.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class NumUtils {
-	public static String suijishu(int length) {
-		StringBuffer sb = new StringBuffer("");
-		Random ra = new Random();
-		for (int i = 0; i < length; i++) {
-			int num = ra.nextInt(10);
-			sb.append(num);
-		}
+    public static String suijishu(int length) {
+        StringBuffer sb = new StringBuffer("");
+        Random ra = new Random();
+        for (int i = 0; i < length; i++) {
+            int num = ra.nextInt(10);
+            sb.append(num);
+        }
 
-		return sb.toString();
-	}
+        return sb.toString();
+    }
+
+    public static String getOrderNum() {
+
+        String format = "yyyyMMddHHmm";
+
+        SimpleDateFormat sf = new SimpleDateFormat(format);
+
+        return sf.format(new Date());
+
+    }
 }
