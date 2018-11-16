f6bbe9e51ac078f37cb191ac85d89e4390dc02b6
johnny <654499437@qq.com>
2018-11-16 17:33:17 +0800

    代码优化

--- framework-parent/framework-web/src/main/java/kim/sesame/framework/web/context/UserContext.java
+++ framework-parent/framework-web/src/main/java/kim/sesame/framework/web/context/UserContext.java
@@ -17,6 +17,7 @@ public class UserContext {
     private final static ThreadLocal<IUser> USER_THREAD_LOCAL = new ThreadLocal<>();
     private final static ThreadLocal<String> SESSIONID = new ThreadLocal<>();
     private final static ThreadLocal<List<IRole>> USER_ROLE = new ThreadLocal<>();
+    private final static ThreadLocal<Object> USER_DATA = new ThreadLocal<>();
 
     //构造器私有化
     private UserContext() {
@@ -66,11 +67,25 @@ public class UserContext {
         USER_ROLE.set(roles);
     }
 
+    /*-------------------------------------------------------*/
+    public Object getData() {
+        return USER_DATA.get();
+    }
+
+    public <T> T getData(Class<T> objectType) {
+        return (T) USER_DATA.get();
+    }
+
+    public void setData(Object data) {
+        USER_DATA.set(data);
+    }
+
 
     /*-------------------------------------------------------*/
     public void clean() {
         USER_THREAD_LOCAL.remove();
         SESSIONID.remove();
         USER_ROLE.remove();
+        USER_DATA.remove();
     }
 }
