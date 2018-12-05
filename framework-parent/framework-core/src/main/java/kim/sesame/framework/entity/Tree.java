3373956e8d2b9c8917ee94282af8a07391615ddc
johnny <654499437@qq.com>
2018-12-05 11:58:55 +0800

    添加节点数据

--- framework-parent/framework-core/src/main/java/kim/sesame/framework/entity/Tree.java
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/entity/Tree.java
@@ -4,19 +4,19 @@ import lombok.Data;
 
 /**
  * 树结构数据
+ *
  * @author johnny
  * date :  2016年1月17日 下午4:17:39
  */
 @Data
 public class Tree implements java.io.Serializable {
-	
-	private String id;//数据的id
-	private String code;
-	private String parentCode;
-	private String name;
-	private boolean checked; //是否选中
-	private boolean open; //是否展开
-	private boolean chkDisabled;//是否禁用,true 禁用
 
-	
+    private String id;//数据的id
+    private String code; // 数据code
+    private String parentCode; // 数据父节点code
+    private String name; // 数据名称
+    private boolean checked; //是否选中
+    private boolean open; //是否展开
+    private boolean chkDisabled;//是否禁用,true 禁用
+    private String basicData; //自定义数据
 }
