d3747cec5829af55f3b42d79e04822b4b761c82d
johnny <654499437@qq.com>
2019-02-13 17:58:59 +0800

    mybatis 模块修改

--- framework-parent/framework-mybatis/src/main/java/kim/sesame/framework/mybatis/config/MybatisConfig.java
+++ framework-parent/framework-mybatis/src/main/java/kim/sesame/framework/mybatis/config/MybatisConfig.java
@@ -2,15 +2,10 @@ package kim.sesame.framework.mybatis.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -25,8 +20,7 @@ import javax.sql.DataSource;
  * Description:
  */
 @Configuration
-@EnableTransactionManagement
-public class MybatisConfig implements TransactionManagementConfigurer {
+public class MybatisConfig{
 	@Resource(name = "dataSource")
 	DataSource dataSource;
 
@@ -65,17 +59,9 @@ public class MybatisConfig implements TransactionManagementConfigurer {
 	 * @param sqlSessionFactory
 	 * @return
 	 */
-	@Bean(name = "sqlSessionTemplate")
-	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}
+//	@Bean(name = "sqlSessionTemplate")
+//	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+//		return new SqlSessionTemplate(sqlSessionFactory);
+//	}
 
-	/**
-	 * 事务管理,具体使用在service层加入@Transactional注解
-	 */
-	@Bean(name = "transactionManager")
-	@Override
-	public PlatformTransactionManager annotationDrivenTransactionManager() {
-		return new DataSourceTransactionManager(dataSource);
-	}
 }
