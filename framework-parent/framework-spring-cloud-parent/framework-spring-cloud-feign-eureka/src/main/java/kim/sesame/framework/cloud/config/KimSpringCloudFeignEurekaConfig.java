7c094c6c045ff5440962bdad3418d201663ce19f
johnny <65449943@qq.com>
2019-02-19 16:47:13 +0800

    spring cloud 模块整改

---
+++ framework-parent/framework-spring-cloud-parent/framework-spring-cloud-feign-eureka/src/main/java/kim/sesame/framework/cloud/config/KimSpringCloudFeignEurekaConfig.java
@@ -0,0 +1,9 @@
+package kim.sesame.framework.cloud.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class KimSpringCloudFeignEurekaConfig {
+}
