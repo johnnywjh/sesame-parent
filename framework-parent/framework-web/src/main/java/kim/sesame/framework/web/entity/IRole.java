1cf9060239441633c3e4c2c12579ab10c1f229a7
johnny <654499437@qq.com>
2019-03-08 21:42:21 +0800

    修改用户和角色实体

--- framework-parent/framework-web/src/main/java/kim/sesame/framework/web/entity/IRole.java
+++ framework-parent/framework-web/src/main/java/kim/sesame/framework/web/entity/IRole.java
@@ -13,13 +13,6 @@ public class IRole extends BaseEntity {
     private String code;//角色编号 唯一
     private String name;//角色名称
     private Integer grade;//角色等级
-    private String remark;//备注
-    private String notdele;//是否允许删除 : Y/N
-    private String active;//是否有效 : Y/N
-
-    // not database field ...
-    private String roles; // 角色集合
-    private int userCount;// 用户个数
 
     @Override
     public String toString() {
