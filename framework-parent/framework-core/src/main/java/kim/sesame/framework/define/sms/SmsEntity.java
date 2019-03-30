6776dff5f7a79717b549d706a60cce58f3abe2de
johnny <654499437@qq.com>
2019-03-30 16:54:08 +0800

    短信模块 重写,

---
+++ framework-parent/framework-core/src/main/java/kim/sesame/framework/define/sms/SmsEntity.java
@@ -0,0 +1,32 @@
+package kim.sesame.framework.define.sms;
+
+import kim.sesame.framework.define.notice.NoticeEntity;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SmsEntity extends NoticeEntity {
+
+    private String phone;//手机号
+    private String templateCode;//模板编号
+    private String templateParam;//模板参数,发送内容
+    private Date sendTime;//发送时间
+
+    public SmsEntity() {
+    }
+
+    public SmsEntity(String phone, String templateCode, String templateParam) {
+        this.phone = phone;
+        this.templateCode = templateCode;
+        this.templateParam = templateParam;
+        this.sendTime = new Date();
+    }
+
+    public SmsEntity(String phone, String templateCode, String templateParam, Date sendTime) {
+        this.phone = phone;
+        this.templateCode = templateCode;
+        this.templateParam = templateParam;
+        this.sendTime = sendTime;
+    }
+}
