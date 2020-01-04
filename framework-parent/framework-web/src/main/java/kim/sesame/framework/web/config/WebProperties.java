07d813808b82982ba048c8a1f75cc1b98f19e593
johnny <654499437@qq.com>
2020-01-04 11:56:11 +0800

    修改icon默认配置

--- framework-parent/framework-web/src/main/java/kim/sesame/framework/web/config/WebProperties.java
+++ framework-parent/framework-web/src/main/java/kim/sesame/framework/web/config/WebProperties.java
@@ -126,7 +126,7 @@ public class WebProperties implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         if (StringUtil.isEmpty(this.iconPath)) {
-            this.iconPath = this.basePath + "/ico/icon.png";
+            this.iconPath = this.basePath + "/icon/icon.png";
         }
         GPage.DEFAULT_PAGE_SIZE = this.defaultPageSize;
         ProjectConfig.setSysCode(this.sysCode);
