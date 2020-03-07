0758af42f2f796a6327f8c0468d81364b20e2d46
johnny <654499437@qq.com>
2020-03-07 20:52:22 +0800

    整合pagehelp

--- framework-parent/framework-mybatis-plus/src/main/java/kim/sesame/framework/db/utils/PageUtil.java
+++ framework-parent/framework-mybatis-plus/src/main/java/kim/sesame/framework/db/utils/PageUtil.java
@@ -1,24 +1,24 @@
 package kim.sesame.framework.db.utils;
 
-//import com.github.pagehelper.Page;
-//import kim.sesame.framework.entity.GPage;
+import com.github.pagehelper.Page;
+import kim.sesame.framework.entity.GPage;
 
 public class PageUtil {
 
-//    public static GPage recount(Page pages) {
-//        return recount(null, pages);
-//    }
-//
-//    public static GPage recount(GPage gPage, Page pages) {
-//        if (gPage == null) {
-//            gPage = new GPage();
-//        }
-//        gPage.setPageNum(pages.getPageNum());
-//        gPage.setPageSize(pages.getPageSize());
-//        gPage.setTotal(pages.getTotal());
-//        gPage.setPages(pages.getPages());
-//        gPage.setCount(pages.isCount());
-//        return gPage;
-//    }
+    public static GPage recount(Page pages) {
+        return recount(null, pages);
+    }
+
+    public static GPage recount(GPage gPage, Page pages) {
+        if (gPage == null) {
+            gPage = new GPage();
+        }
+        gPage.setPageNum(pages.getPageNum());
+        gPage.setPageSize(pages.getPageSize());
+        gPage.setTotal(pages.getTotal());
+        gPage.setPages(pages.getPages());
+        gPage.setCount(pages.isCount());
+        return gPage;
+    }
 
 }
