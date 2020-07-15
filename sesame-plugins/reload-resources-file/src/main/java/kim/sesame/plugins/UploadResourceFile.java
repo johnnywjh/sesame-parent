ececf94de7614c08ffc5b78cd6b5e40ae2c65039
johnny <654499437@qq.com>
2020-07-15 18:05:25 +0800

    1

---
+++ sesame-plugins/reload-resources-file/src/main/java/kim/sesame/plugins/UploadResourceFile.java
@@ -0,0 +1,82 @@
+package kim.sesame.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@Mojo(name = "reloadResourcesFile")
+public class UploadResourceFile extends AbstractMojo {
+
+    /**
+     * 源码文件夹
+     */
+    @Parameter
+    private List<String> srcFiles;
+
+    /**
+     * 目标文件夹
+     */
+    @Parameter
+    private boolean updateAll = false;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        getLog().info("更新资源文件 开始..................");
+        if (srcFiles != null && srcFiles.size() > 0) {
+
+            getLog().info("--------------");
+            List<String> zips = new ArrayList<>();
+            try {
+                for (int i = 0; i < srcFiles.size(); i++) {
+                    diffAndCover(new File(srcFiles.get(i)));
+                }
+            } catch (Exception e) {
+                getLog().error(e.getMessage());
+            }
+            getLog().info("--------------");
+        } else {
+            getLog().warn("没有要处理的文件夹目录");
+        }
+        getLog().info("更新资源文件 结束..................");
+    }
+
+    /**
+     * 比较并覆盖
+     */
+    public void diffAndCover(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                diffAndCover(files[i]);
+            }
+        } else {
+            String srcPath = file.getAbsolutePath();  // 源代码文件目录
+            String targetPath = srcPath.replace("src/main/resources", "target/classes")
+                    .replace("src\\main\\resources", "target\\classes");
+            File targetFile = new File(targetPath);
+
+            try {
+                if (updateAll) {
+                    FileUtils.copyFile(file, targetFile);
+                    getLog().info("文件更新:" + srcPath);
+                } else {
+                    if (file.length() != targetFile.length()) {
+                        FileUtils.copyFile(file, targetFile);
+                        getLog().info("文件更新:" + srcPath);
+                    }
+                }
+
+            } catch (Exception e) {
+                getLog().error(e.getMessage());
+            }
+
+        }
+    }
+
+}
