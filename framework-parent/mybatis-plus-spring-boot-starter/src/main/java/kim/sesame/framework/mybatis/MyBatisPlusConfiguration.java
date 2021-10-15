0844e7dc6220426735252ad3598454738d5e0317
wangjianghai <wangjianhai123>
2021-10-15 18:03:34 +0800

    修改配置

--- framework-parent/mybatis-plus-spring-boot-starter/src/main/java/kim/sesame/framework/mybatis/MyBatisPlusConfiguration.java
+++ framework-parent/mybatis-plus-spring-boot-starter/src/main/java/kim/sesame/framework/mybatis/MyBatisPlusConfiguration.java
@@ -19,15 +19,6 @@ public class MyBatisPlusConfiguration {
     // #mybatis-plus.mapper-locations=classpath*:**/dao/*DaoMapping.xml
     // mybatis-plus.mapper-locations=classpath:/mapper/**/*.xml
 
-    /****
-     * 获取数据源对象 DruidDataSource
-     */
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource druidDataSource(){
-//        return new DruidDataSource();
-//    }
-
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,
      * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
@@ -50,4 +41,47 @@ public class MyBatisPlusConfiguration {
         };
     }
 
+/*
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import java.util.HashMap;
+import java.util.Map;
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DruidDataSource druidDataSource(){
+        return new DruidDataSource();
+    }
+    //配置监控servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet() , "/druid/*");
+        Map<String,String> initParameter= new HashMap<>();
+        initParameter.put("loginUsername" ,"admin");
+        initParameter.put("loginPassword" ,"123456");
+        initParameter.put("allow" ,"");//默认允许所有访问
+        // 添加IP白名单
+        //servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        // 添加IP黑名单，当白名单和黑名单重复时，黑名单优先级更高
+        //servletRegistrationBean.addInitParameter("deny", "127.0.0.1");
+        // 是否能够重置数据
+        bean.addInitParameter("resetEnable", "false");
+        bean.setInitParameters(initParameter);
+        return bean;
+    }
+    //配置监控filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean(new WebStatFilter());
+        // 添加过滤规则
+        bean .addUrlPatterns("/*");
+        // 忽略过滤格式
+        bean .addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*,");
+        return bean;
+    }
+*/
 }
