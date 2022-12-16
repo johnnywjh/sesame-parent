efab3d77a6ece600cb80d1bfc1d1172c644d29ea
wangjianghai <654499437@qq.com>
2022-12-16 13:57:02 +0800

    添加注释

--- framework-define/src/main/java/kim/sesame/common/web/AbstractRequest.java
+++ framework-define/src/main/java/kim/sesame/common/web/AbstractRequest.java
@@ -45,6 +45,14 @@ public abstract class AbstractRequest extends PrintFriendliness {
         this.appId = appId;
     }
 
+    /*
+     * https://blog.csdn.net/m0_46360888/article/details/116004078
+     * @NotEmpty :不能为null，且Size>0，一般用于集合、数组、字符序列
+     * @NotNull:不能为null，但可以为empty,没有Size的约束，一般用于包装类型判断
+     * @NotBlank:只用于String,不能为null且trim()之后size>0，一般用于字符串
+     * @Length：java中的length()方法是针对字符串String说的,如果想看这个字符串的长度则用到length()这个方法.一般用于字符型长度判断
+     * @Size：java中的size()方法是针对泛型集合说的,如果想看这个泛型有多少个元素,一般用于集合、数组、字符序列长度的判断
+     */
     public void validate() {
         StringBuilder errorMsgs = new StringBuilder();
         Set<ConstraintViolation<AbstractRequest>> violations = VALIDATOR.validate(this);
@@ -59,26 +67,29 @@ public abstract class AbstractRequest extends PrintFriendliness {
 
     /**
      * requestId是否强制必填
+     *
      * @return
      */
-    public boolean requiresRequestId( ) {
+    public boolean requiresRequestId() {
         return false;
     }
 
     /**
      * appId是否强制必填
+     *
      * @return
      */
-    public boolean requiresAppId( ) {
+    public boolean requiresAppId() {
         return false;
     }
 
     /**
      * 构造该请求的日志上下文标识, 默认是 类名+requestId
+     *
      * @param
      * @return
      */
-    public String buildLogPrefix( ) {
+    public String buildLogPrefix() {
         if (StringUtils.isBlank(requestId)) {
             setRequestId(UUIDUtil.getShortUUID());
         }
@@ -87,9 +98,10 @@ public abstract class AbstractRequest extends PrintFriendliness {
 
     /**
      * 是否需要打印请求日志
+     *
      * @return
      */
-    public boolean needsLog( ) {
+    public boolean needsLog() {
         return true;
     }
 }
