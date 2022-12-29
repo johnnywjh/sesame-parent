c3be7d9638769a34bb074bb05538fd2135e56a12
johnny <654499437@qq.com>
2022-12-29 13:35:31 +0800

    修改 loginUserId

--- web-spring-boot-starter/src/main/java/kim/sesame/common/web/config/CorsConfig.java
+++ web-spring-boot-starter/src/main/java/kim/sesame/common/web/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package kim.sesame.common.web.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,10 +18,7 @@ public class CorsConfig {
         return corsConfiguration;
     }
 
-    /**
-     * 如果在网关里加入了跨域,这里就不需要了
-     */
-//    @Bean
+    @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig()); // 4
