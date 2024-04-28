20314d9e132ebf7f86ab75fc9daa38ce0b9509e8
wangjianghai <wangjianghai@rz-ioc.com>
2024-04-28 10:05:51 +0800

    文件下载修改-优化

--- web-spring-boot-starter/src/main/java/kim/sesame/common/web/controller/AbstractWebController.java
+++ web-spring-boot-starter/src/main/java/kim/sesame/common/web/controller/AbstractWebController.java
@@ -1,19 +1,23 @@
 package kim.sesame.common.web.controller;
 
-import cn.hutool.core.io.resource.ResourceUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.write.builder.ExcelWriterBuilder;
 import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * Web controller基类
@@ -23,13 +27,13 @@ public class AbstractWebController extends AbstractController {
 
     private static final String FILE_NAME_KEY = "attachment;filename=";
 
-    public void download(String fileName, InputStream inputStream, HttpServletRequest request, HttpServletResponse response) {
+    public void download(String fileName, String path, HttpServletRequest request, HttpServletResponse response) {
         try {
+
             setDownloadResponse(fileName, request, response);
 
             OutputStream outputStream = response.getOutputStream();
-//            InputStream inputStream = new FileInputStream(path);
-//            InputStream inputStream = ResourceUtil.getStream(path);
+            InputStream inputStream = new FileInputStream(path);
 //            log.info("inputStream.available()={}", inputStream.available());
 
             byte[] b = new byte[inputStream.available()];
@@ -75,13 +79,17 @@ public class AbstractWebController extends AbstractController {
     }
 
 
+    /**
+     * 表格数据导出
+     */
     public void tableDataExport(String fileName, List list, Class clazz, HttpServletResponse response) {
         tableDataExport(fileName, list, clazz, response, "1", true);
     }
 
-    /**
-     * 表格数据导出
-     */
+    public void tableDataExport(String fileName, List list, Class clazz, HttpServletResponse response, String sheetName) {
+        tableDataExport(fileName, list, clazz, response, sheetName, true);
+    }
+
     public void tableDataExport(String fileName, List list, Class clazz, HttpServletResponse response, String sheetName, boolean autoWrite) {
 
         try {
@@ -115,5 +123,41 @@ public class AbstractWebController extends AbstractController {
         } catch (Exception e) {
             log.error("表格导出异常异常 {}:", fileName, e);
         }
+    }
+
+    public ResponseEntity<?> tableDataExportGzip(String fileName, List list, Class clazz) {
+        return tableDataExportGzip(fileName, list, clazz, "1", true);
+    }
+
+    public ResponseEntity<?> tableDataExportGzip(String fileName, List list, Class clazz, String sheetName) {
+        return tableDataExportGzip(fileName, list, clazz, sheetName, true);
+    }
+
+    public ResponseEntity<?> tableDataExportGzip(String fileName, List list, Class clazz, String sheetName, boolean autoWrite) {
+        // 创建一个内存流，用于保存压缩后的数据
+        if (!list.isEmpty()) {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            // 压缩数据
+            try (GZIPOutputStream gzipOut = new GZIPOutputStream(os)) {
+
+                ExcelWriterBuilder write = EasyExcel.write(gzipOut, clazz);
+                if (autoWrite) {
+                    write.registerWriteHandler(new LongestMatchColumnWidthStyleStrategy());
+                }
+                write.sheet(sheetName).doWrite(list);
+
+                fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.displayName());
+
+                return ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
+                        .header(HttpHeaders.CONTENT_ENCODING, "gzip")
+                        .header(HttpHeaders.CONTENT_DISPOSITION, FILE_NAME_KEY + fileName)
+                        .body(os.toByteArray());
+            } catch (Exception e) {
+                return ResponseEntity.status(500).body(e.getMessage());
+            }
+        }
+        return ResponseEntity.ok().build();
     }
 }
