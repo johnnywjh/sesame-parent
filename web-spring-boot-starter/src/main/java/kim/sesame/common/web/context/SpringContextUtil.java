f20d5b953b5e4d420ce41a1892ab5d976abe2bfa
wangjianghai <wangjianghai@rz-ioc.com>
2024-05-09 22:59:25 +0800

    优化

--- web-spring-boot-starter/src/main/java/kim/sesame/common/web/context/SpringContextUtil.java
+++ web-spring-boot-starter/src/main/java/kim/sesame/common/web/context/SpringContextUtil.java
@@ -1,104 +1,15 @@
 package kim.sesame.common.web.context;
 
+import cn.hutool.extra.spring.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
-import java.util.Map;
 
 @Component
 @Slf4j
-public class SpringContextUtil implements ApplicationContextAware {
-
-    private static ApplicationContext applicationContext;
-    private static String active;
-
-    public static String getActive() {
-        return active;
-    }
-
-    /**
-     * 获取上下文
-     */
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
-
-    /**
-     * 设置上下文
-     */
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        SpringContextUtil.applicationContext = applicationContext;
-        active = getAttributeValue("spring.profiles.active");
-    }
-
-    /**
-     * 动态注入bean, 当前bean一定要有无参的构造函数
-     *
-     * @param beanName bean的名字
-     * @param params   bean的属性
-     * @param clazz    bean 的clazz
-     */
-    public static void registerBean(String beanName, Map<String, Object> params, Class clazz) {
-        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(clazz);
-        for (String key : params.keySet()) {
-            beanDefinitionBuilder.addPropertyValue(key, params.get(key));
-        }
-
-        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) getApplicationContext().getAutowireCapableBeanFactory();
-        beanFactory.registerBeanDefinition(beanName, beanDefinitionBuilder.getBeanDefinition());
-    }
-
-    /**
-     * 通过名字获取上下文中的bean
-     *
-     * @param name beanName
-     * @return object
-     */
-    public static Object getBean(String name) {
-        try {
-            return applicationContext.getBean(name);
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    /**
-     * 通过类型获取上下文中的bean
-     *
-     * @param requiredType className
-     * @return object
-     */
-    public static <T> T getBean(Class<T> requiredType) {
-        try {
-            return applicationContext.getBean(requiredType);
-        } catch (Exception e) {
-            return null;
-        }
-
-    }
-
-    /**
-     * 获取application 里的属性值
-     *
-     * @param key key
-     * @return str
-     */
-    public static String getAttributeValue(String key) {
-        try {
-            return applicationContext.getEnvironment().getProperty(key);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-
-    }
+public class SpringContextUtil extends SpringUtil {
 
     public static void printStartInfo(){
         printStartInfo(null);
@@ -110,9 +21,9 @@ public class SpringContextUtil implements ApplicationContextAware {
             }
         } catch (Exception e) {
         }
-//        String active = getAttributeValue("spring.profiles.active");
-        String port = getAttributeValue("server.port");
-        String path = getAttributeValue("server.servlet.context-path");
+        String active = getProperty("spring.profiles.active");
+        String port = getProperty("server.port");
+        String path = getProperty("server.servlet.context-path");
         path = StringUtils.isNotBlank(path) ? path : "";
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application is running! Access URLs:\n\t" +
@@ -122,6 +33,93 @@ public class SpringContextUtil implements ApplicationContextAware {
                 "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
                 "----------------------------------------------------------");
     }
-
-
 }
+//import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+//import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.ApplicationContextAware;
+//public class SpringContextUtil extends ApplicationContextAware {
+//    private static ApplicationContext applicationContext;
+//    private static String active;
+//
+//    public static String getActive() {
+//        return active;
+//    }
+//
+//    /**
+//     * 获取上下文
+//     */
+//    public static ApplicationContext getApplicationContext() {
+//        return applicationContext;
+//    }
+//
+//    /**
+//     * 设置上下文
+//     */
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) {
+//        SpringContextUtil.applicationContext = applicationContext;
+//        active = getAttributeValue("spring.profiles.active");
+//    }
+//
+//    /**
+//     * 动态注入bean, 当前bean一定要有无参的构造函数
+//     *
+//     * @param beanName bean的名字
+//     * @param params   bean的属性
+//     * @param clazz    bean 的clazz
+//     */
+//    public static void registerBean(String beanName, Map<String, Object> params, Class clazz) {
+//        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(clazz);
+//        for (String key : params.keySet()) {
+//            beanDefinitionBuilder.addPropertyValue(key, params.get(key));
+//        }
+//
+//        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) getApplicationContext().getAutowireCapableBeanFactory();
+//        beanFactory.registerBeanDefinition(beanName, beanDefinitionBuilder.getBeanDefinition());
+//    }
+//
+//    /**
+//     * 通过名字获取上下文中的bean
+//     *
+//     * @param name beanName
+//     * @return object
+//     */
+//    public static Object getBean(String name) {
+//        try {
+//            return applicationContext.getBean(name);
+//        } catch (Exception e) {
+//            return null;
+//        }
+//    }
+//
+//    /**
+//     * 通过类型获取上下文中的bean
+//     *
+//     * @param requiredType className
+//     * @return object
+//     */
+//    public static <T> T getBean(Class<T> requiredType) {
+//        try {
+//            return applicationContext.getBean(requiredType);
+//        } catch (Exception e) {
+//            return null;
+//        }
+//
+//    }
+//
+//    /**
+//     * 获取application 里的属性值
+//     *
+//     * @param key key
+//     * @return str
+//     */
+//    public static String getAttributeValue(String key) {
+//        try {
+//            return applicationContext.getEnvironment().getProperty(key);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//
+//    }
